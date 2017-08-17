d664b3f7e4a8cf03c05648d05ec7a1375e6c0752
Anton P <poster.instant@gmail.com>
2017-08-17 17:10:38 +0300

    new branch add temp, add new activity

---
+++ app/src/main/java/com/example/poster/easysellingsellers/activity/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.example.poster.easysellingsellers.activity;
+
+;import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 017 17.08.17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+
+}
---
+++ app/src/main/java/com/example/poster/easysellingsellers/activity/NavigationDrawerActivity.java
@@ -0,0 +1,9 @@
+package com.example.poster.easysellingsellers.activity;
+
+/**
+ * Created by User on 017 17.08.17.
+ */
+
+public class NavigationDrawerActivity extends BaseActivity{
+
+}
